2552cb86167c26cd0ed14c57641b0c95f6c09078
sorin.lapadus <sorin.lapadus@indsoft.ro>
2023-06-22 20:31:16 +0300

    Add dynamic table

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import "./style.css";
 console.warn("Starting app...");
 
+function $(selector) {
+  return document, document.querySelector(selector);
+}
+
+function getTeamAsHTML(team) {
+  return `<tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.projectName}</td>
+    <td>${team.projectURL}</td>
+    <td>x &#9998;</td>
+    </tr>`;
+}
+
+function displayTeams(teams) {
+  const teamsHTML = teams.map(getTeamAsHTML);
+  $("#teamsTable tbody").innerHTML = teamsHTML.join("");
+}
+
 function loadTeams() {
-  const request = fetch("teams.json");
-  const response = request.then(r => r.json);
-  response.then(result => {
-    console.warn("result", result);
-  });
+  fetch("teams.json")
+    .then(r => r.json())
+    .then(teams => {
+      displayTeams(teams);
+    });
 }
 loadTeams();
