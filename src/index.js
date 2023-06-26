973c7e6e3734d411e0c979ee5243d7fda9db5cf8
sorin.lapadus <sorin.lapadus@indsoft.ro>
2023-06-26 19:14:18 +0300

    Implement the delete button

--- src/index.js
+++ src/index.js
@@ -9,18 +9,27 @@ function getTeamAsHTML(team) {
   return `<tr>
     <td>${team.promotion}</td>
     <td>${team.members}</td>
-    <td>${team.projectName}</td>
-    <td>${team.projectURL}</td>
+    <td>${team.name}</td>
+    <td>${team.url}</td>
     <td>
-    <a class="delete-btn">x</a>
-    <a class="edit-btn">&#9998;</a>
+    <a data-id="${team.id}" class="remove-btn">x</a>
+    <a data-id="${team.id}" class="edit-btn">&#9998;</a>
     </td>
     </tr>`;
 }
 
 function initEvents() {
   const tbody = $("#teamsTable tbody").addEventListener("click", e => {
-    console.warn(e.target);
+    if (e.target.matches("a.remove-btn")) {
+      const id = e.target.dataset.id;
+      console.warn("will remove %o", id);
+      deleteTeamRequest(id).then(status => {
+        if (status.success) {
+          console.warn("delete done", status);
+          loadTeams();
+        }
+      });
+    }
   });
 }
 
@@ -40,6 +49,16 @@ function loadTeams() {
     .then(teams => {
       displayTeams(teams);
     });
+}
+
+function deleteTeamRequest(id) {
+  return fetch("http://localhost:3000/teams-json/delete", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ id: id })
+  }).then(r => r.json());
 }
 initEvents();
 loadTeams();
